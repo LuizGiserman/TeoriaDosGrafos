275e78ea3ed61c53e0d3c2e1cc4a6ff03d8d6be9
Luiz Giserman <luizgiserman@poli.ufrj.br>
2019-11-15 17:41:39 -0300

    files back

---
+++ grafos.h
@@ -0,0 +1,166 @@
+/*
+* Universidade Federal do Rio de Janeiro
+* Autores: Luiz Giserman e Renan Neri
+* Disciplina: Teoria dos Grafos
+*/
+
+#ifndef GRAFOS_H
+#define GRAFOS_H
+
+
+#include <iostream>
+#include <string>
+#include <cerrno>
+#include <vector>
+#include <bitset>
+#include <list>
+#include <fstream>
+#include <cstring>
+#include <stack>
+#include <utility>
+
+#define READINGS_SPT            2
+#define ERROR_READING_FILE      1
+
+#define INFINITY                std::numeric_limits<int>::max()
+
+#define LIST_TYPE               0
+#define MATRIX_TYPE             1
+
+
+
+typedef unsigned char BYTE;
+typedef std::pair< int, int> dist_vertice;
+using namespace std;
+
+class Edge
+{
+    public:
+    Edge(int connectedVertice, int weight);
+    int connectedVertice;
+    float weight = 1;
+};
+
+class Vertice
+{
+    public:
+
+        int type;
+        int size;
+        bool hasWeight;
+
+        vector <int> adjList; /*type = 0*/
+        vector <Edge> adjListWeight;
+
+        vector < bitset<1> > adjRow; /*type = 1*/
+        vector <float> adjRowWeight;
+
+        Vertice(int type, int size, bool hasWeight);
+        void setVertice(int Vertice, float auxWeight = 0);
+        bool HasEdge(int numVertice);
+};
+
+class Components
+{
+  public:
+    Components();
+    void InsertVertice(int numVertice);
+    int size;
+    list<int> listComp;
+
+    bool compare(const Components &comp1, const Components &comp2);
+};
+
+class Grafos
+{
+
+public:
+  Grafos(string fileName, int type);
+  ~Grafos();
+  vector<Vertice> grafo;
+
+  int numGroup1;
+  int numGroup2;
+  vector<int> group1;
+  vector<int> group2;
+
+  bool hasWeight;
+  bool allPos = true;
+  string filename;
+  int type;
+  int numVertices;
+  int numEdges;
+  int maxDegree;
+  int minDegree;
+  int avgDegree;
+  int medDegree;
+
+  /*funcoes essenciais*/
+  void CreateGrafo(); /*cria os vertices e os coloca em grafo*/
+  void Populate(); /*Insere os vértices vizinhos nas listas de adjacências*/
+  void BFS(int initialVertice, int search);
+  void DFS(int initialVertice, int search);
+  void ConnectedComponents(int search);
+  void GetDiameter();
+  void Distance(int firstVertice, int secondVertice); /*dist entre 2 vertices*/
+
+  /*Overload for the Distance between 2 specific vertices function*/
+  float Dijkstra (int initialVertice, int secondVertice, vector <int> &father);
+
+  /*Overload for the regular Dijkstra (getting distances) */
+  void Dijkstra (int initialVertice, vector <float> &distance);
+
+  /*Overload for simple (prints fathers, doesnt returnt it) Dijkstra*/
+  void Dijkstra (int initialVertice);
+
+  /*Overload: return fathers*/
+  void Dijkstra (int initialVertice, vector <int> &father);
+
+  float Dijkstra (int initialVertice, vector <int> &father, vector <float> &distance, int secondVertice);
+
+  void Prim ();
+  void PrintAllPaths (int initialVertice);
+
+  /*funções auxiliares*/
+  void Print(); /*Imprime a estrutura do grafo, independente de qual for*/
+  void PrintInformation(); /*Imprime informações sobre o grafo*/
+
+  bool isBipartite();
+
+  int maximumBipartiteMatching();
+
+  void BellmanFord();
+
+private:
+
+  void BFSGenerica(
+    int initialVertice,
+    int **BFSinfo,
+    Components *auxComponent = NULL,
+    int BFStype = 0,
+    int StopVertice = 0,
+    int *diameter = NULL,
+    std::list<int>::iterator *arrayPointer = NULL,
+    list<int> *listVerticesforCC = NULL
+  );
+/* BFSType 0 -> Normal BFS = 1; parada de vértice = 2 ; compenentes conexas = 3 */
+
+  bool isBipartite(int initialVertice, int* color);
+
+  int indexNotVisited(int* color);
+
+  bool augment_path(int vertex, bool *visited, int *matched);
+
+  void BellmanFord(int initialVertice, int* distance);
+
+  void DFSGenerica(
+    int initialVertice,
+    int **father_level
+  );
+
+  void GetInformation();
+  void PrintList();
+  void PrintMatrix();
+
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <sys/timeb.h>
+#include <chrono>
+#include <time.h>
+#include "grafos.h"
+
+
+using namespace std;
+
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+    vm_usage     = 0.0;
+    resident_set = 0.0;
+
+    // the two fields we want
+    unsigned long vsize;
+    long rss;
+    {
+        std::string ignore;
+        std::ifstream ifs("/proc/self/stat", std::ios_base::in);
+        ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
+                >> ignore >> ignore >> vsize >> rss;
+    }
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
+}
+
+int main(){
+
+   std::chrono::steady_clock::time_point start;
+   std::chrono::steady_clock::time_point end;
+   ofstream file;
+   double vm, rss;
+
+   file.open("./result.txt");
+  /*as_graph*/
+
+     Grafos grafo = Grafos("grafoteste2.txt", LIST_TYPE);
+     file << "FileName: " << grafo.filename << endl;
+     bool isbipartite = grafo.isBipartite();
+     file << "e bipartido? " << isbipartite << endl;
+     if (isbipartite){
+       file << "Maximum Matching" << grafo.maximumBipartiteMatching() << endl;
+     }
+
+     file.close();
+
+  return 0;
+}
